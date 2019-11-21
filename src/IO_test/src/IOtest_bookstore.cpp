45c5bdabe2ff965407b6888e04ff1edc2d2bbfb4
Shi Wei <32674047+SeeeeShiwei@users.noreply.github.com>
2019-11-21 20:53:06 +0800

    练习8.6_处理书店销售记录文件

---
+++ src/IO_test/src/IOtest_bookstore.cpp
@@ -0,0 +1,72 @@
+//============================================================================
+// Name        : IOtest_bookstore.cpp
+// Author      : Sesen
+// Version     :
+// Copyright   : Your copyright notice
+// Description : 练习8.6：读取销售记录文件，当连续卖出相同的书时，进行汇总，并写入文件。
+//============================================================================
+
+
+#include<fstream>
+#include<vector>
+#include <iostream>
+#include <string>
+using namespace std;
+#include "Sales_data.h"
+
+void process_sold_record(string infile, string outfile)
+{
+	ifstream input(infile);
+	ofstream output(outfile);
+	Sales_data total;
+
+	if (read(input,total))
+	{
+        Sales_data trans;
+        while(read(input,trans))
+        {
+        	if (total.isbn() == trans.isbn())
+        		total.combine(trans);
+        	else
+        	{
+        		print(output,total) << endl; // output此时为文件写入流，直接写入文件，而非打印至输出台
+        		total = trans;
+        	}
+        }
+        print(output,total);
+	}
+	else
+		cerr << "No data" << endl;
+}
+
+int main()
+{
+    string sold_file;
+    string out_file;
+    sold_file = "soldbook.txt";
+    out_file = "output.txt";
+    process_sold_record(sold_file,out_file);
+
+	return 0;
+}
+
+/*
+ * 输入
+ 撒哈拉沙漠  3  25.0
+撒哈拉沙漠 5 20.0
+百面机器学习 2 40.0
+Linux从入门到放弃 2 89.0
+数据结构 4 56.0
+百面机器学习 1 30.0
+ */
+
+/*
+ * 输出
+ 书名： 撒哈拉沙漠 售出数量： 8  销售总额： 175 售出单价:21.875
+书名： 百面机器学习 售出数量： 2  销售总额： 80 售出单价:40
+书名： Linux从入门到放弃 售出数量： 2  销售总额： 178 售出单价:89
+书名： 数据结构 售出数量： 4  销售总额： 224 售出单价:56
+书名： 百面机器学习 售出数量： 1  销售总额： 30 售出单价:30
+ *
+ */
+
---
+++ src/IO_test/src/Sales_data.h
@@ -0,0 +1,61 @@
+/*
+ * Sales_data.h
+ *
+ *  Created on: 2019年10月31日
+ *      Author: 石威（Sesen）
+ */
+
+#ifndef SALES_DATA_H_
+#define SALES_DATA_H_
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Sales_data
+{
+// 为Sales_data的非成员函数做友元声明
+friend Sales_data add(const Sales_data&, const Sales_data&);
+friend istream &read(istream&, Sales_data&);
+friend ostream &print(ostream&,const Sales_data&);
+//访问说明符
+public:
+	//构造函数
+	Sales_data() = default;
+	Sales_data(const string &s): bookno(s) {};
+	Sales_data(const string &s, unsigned n, double p):
+		bookno(s),units_sold(n),total_money(p*n) {};
+	Sales_data(istream&);
+
+    Sales_data &combine(const Sales_data &rhs)
+    {
+        units_sold += rhs.units_sold;
+        total_money += rhs.total_money;
+        return *this;
+    }
+    string isbn() const  {return bookno;}
+
+
+//私有访问说明符，类内数据成员和实现部分函数
+private:
+	double avg_price() const;
+
+	string bookno;
+    unsigned units_sold = 0;
+    double total_money = 0.0;
+};
+
+//Sales_data接口的非成员函数声明，定义在utils.cpp中，因声明了友元函数，所以可以使用private成员
+Sales_data add(const Sales_data&, const Sales_data&);
+istream &read(istream&, Sales_data&);
+ostream &print(ostream&,const Sales_data&);
+
+
+inline
+double Sales_data::avg_price() const
+{
+	return units_sold ? total_money / units_sold : 0;
+}
+
+
+#endif /* SALES_DATA_H_ */
