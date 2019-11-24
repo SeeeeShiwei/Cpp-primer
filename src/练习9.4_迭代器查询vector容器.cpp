ea0f65595a975c789295105b027266e6a05bc52f
Shi Wei <32674047+SeeeeShiwei@users.noreply.github.com>
2019-11-24 19:44:41 +0800

    Create 练习9.4_迭代器查询vector容器.cpp

---
+++ src/练习9.4_迭代器查询vector容器.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : Sequential_Container.cpp
+// Author      : Seseneee
+// Version     :
+// Copyright   : Your copyright notice
+// Description : 练习9.4  使用迭代器，在vector数组中查找target是否存在。
+//============================================================================
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool find_target(vector<int> &nums, int target)
+{
+	bool res = false;
+	for(auto iter = nums.begin();iter < nums.end();iter++)
+	{
+        if(*iter == target) // 迭代器解引用
+        {
+        	res = true;
+        	return res;
+        }
+	}
+	return res;
+}
+int main() {
+	vector<int> nums = {1,23,45,2,123,34,45};
+	int target = 2;
+	bool ans = find_target(nums,target);
+	cout << boolalpha << ans << endl; // 输出bool值，而非0-1.
+	return 0;
+}
