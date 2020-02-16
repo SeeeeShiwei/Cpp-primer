b31bdbaefec98bb68b09c00b99e710ae1feacf5c
shiwei <shiw_21@163.com>
2020-02-16 16:50:06 +0800

    Create test.cpp
    算法测试

---
+++ src/test.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : Sesen
+// Version     :
+// Copyright   : Your copyright notice
+// Description : test and val
+//============================================================================
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <string>
+#include <list>
+#include <functional>
+
+using namespace std;
+using namespace std::placeholders;
+
+
+
+int main() {
+	vector<int> nums{1, 1, 5, 6, 9, 1,3};
+	
+
+	return 0;
+}
+
+
+
+
